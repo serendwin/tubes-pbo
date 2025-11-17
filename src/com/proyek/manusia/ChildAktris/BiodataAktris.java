02bcd4e51b01e685d08bea97f59b459e53ff6604
serendwin <twentyhours133@gmail.com>
2025-11-18 00:08:32 +0700

    nambahin codingan biodata aktris

--- src/com/proyek/manusia/ChildAktris/BiodataAktris.java
+++ src/com/proyek/manusia/ChildAktris/BiodataAktris.java
@@ -4,40 +4,50 @@ import com.proyek.manusia.Agensi;
 
 public class BiodataAktris extends Agensi {
 
-    private String drama; //properti
-    private int jumlahDrama;
-    private String agensi;
-
-    public BiodataAktris(String nama, int umur, String jenisKelamin, String kewarganegaraan, int tahun,
-                  String drama, int jumlahDrama, String agensi) {
-        super(nama, umur, jenisKelamin, kewarganegaraan, tahun);
-        this.drama = drama;
-        this.jumlahDrama = jumlahDrama;
-        this.agensi = agensi; //constructor
+    public BiodataAktris(
+            String Nama,
+            String Kewarganegaraan,
+            String TtlLahir,
+            String TtlDebut,
+            int MasaTrainee,
+            String Posisi,
+            String ListDrama,
+            String ListMovie,
+            String ListAlbum,
+            String Penghargaan,
+            int tahun,
+            String Event,
+            double Price
+    ) {
+        super(
+                Nama,
+                Kewarganegaraan,
+                TtlLahir,
+                TtlDebut,
+                MasaTrainee,
+                Posisi,
+                ListDrama,
+                ListMovie,
+                ListAlbum,
+                Penghargaan,
+                tahun,
+                Event,
+                Price
+        );
     }
-
-    @Override 
-    public void bekerja() {
-        System.out.println(nama + " sedang syuting drama " + drama);
-    }
-
-    public void news() { //methods
-        System.out.println("Aktris " + nama + " akan bergabung dengan " + drama + " yang sudah dikonfirmasi oleh " + agensi);
-    }
-
-    public void dispatch(boolean berita) {
-        if (berita) {
-            System.out.println(nama + " telah membintangi " + jumlahDrama + " drama selama kariernya.");
-        } else {
-            news();
-        }
-    }
-
-    public void fanMeeting() {
-        System.out.println(nama + " mengadakan fan meeting bersama penggemar di Seoul!");
-    }
-
-    public void wawancara() {
-        System.out.println(nama + " sedang menghadiri acara wawancara bersama media.");
+    
+    public void tampilkanBiodata() {
+        System.out.println("===== Biodata Aktris =====");
+        System.out.println("Nama: " + Nama);
+        System.out.println("Kewarganegaraan: " + Kewarganegaraan);
+        System.out.println("Tanggal Lahir: " + TtlLahir);
+        System.out.println("Tahun Debut: " + TtlDebut);
+        System.out.println("Posisi: " + Posisi);
+        System.out.println("List Drama: " + ListDrama);
+        System.out.println("List Movie: " + ListMovie);
+        System.out.println("List Album: " + ListAlbum);
+        System.out.println("Penghargaan: " + Penghargaan);
+        System.out.println("Tahun: " + tahun);
     }
 }
+
