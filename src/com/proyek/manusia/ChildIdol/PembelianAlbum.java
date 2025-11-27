1d1850ebf8720900cbe576b704ebe750a7439376
Elsa <elsasetiamarcsa2803@gmail.com>
2025-11-27 23:52:56 +0700

    PEBEO

--- src/com/proyek/manusia/ChildIdol/PembelianAlbum.java
+++ src/com/proyek/manusia/ChildIdol/PembelianAlbum.java
@@ -1,101 +1,53 @@
 package com.proyek.manusia.ChildIdol;
 
 import java.util.Scanner;
+import com.proyek.manusia.Agensi;
 
-public class PembelianAlbum extends Idol {
+public class Idol extends Agensi{
 
-    private String album;
-    private double harga;
-    private int qty;
-    private double total;
+    public Idol(String nama, String kewarganegaraan, String ttlLahir,
+                  String ttlDebut, int masaTrainee) {
+        super(nama, kewarganegaraan, ttlLahir, ttlDebut, masaTrainee);}
 
-    public PembelianAlbum(String nama, String kewarganegaraan, String ttlLahir,
-                          String ttlDebut, int masaTrainee, String album) {
-        super(nama, kewarganegaraan, ttlLahir, ttlDebut, masaTrainee);
+    public void tampilkanbiodatagroupidol() {
+        System.out.println("Menampilkan biodata idol");}
+    
+    public void tampilkanbiodatamemberNCTDream() {
+        System.out.println("Menampilkan biodata idol");}
 
-        this.album = album;
-        this.harga = tentukanHarga(album);
+    public void tampilkanbiodatamemberDAY6() {
+        System.out.println("Menampilkan biodata idol");}
+
+    public void infoIdol() {
+        System.out.println("Menampilkan info idol");
     }
 
-    @Override
+    public void infoEvent() {
+        System.out.println("Menampilkan info event");}
+
+    public void inputTiket(Scanner input) {
+        System.out.println("Input tiketing");}
+
+    public void cetakTiket() {
+        System.out.println("Menampilkan tiket");}
+
     public void PricelistAlbumnct() {
-        System.out.println("========== PRICELIST ALBUM NCT DREAM ==========");
-        System.out.println("GLITCH MODE     : Rp " + tentukanHarga("GLITCH MODE"));
-        System.out.println("HOT SAUCE     : Rp " + tentukanHarga("HOT SAUCE"));
-        System.out.println("===============================================");
+        System.out.println("Menampilkan pricelist album NCT DREAM");
     }
 
-    @Override
     public void PricelistAlbumday6() {
-        System.out.println("========== PRICELIST ALBUM DAY6 ==========");
-        System.out.println("FOUREVER     : Rp " + tentukanHarga("FOUREVER"));
-        System.out.println("THE BOOK OF US     : Rp " + tentukanHarga("THE BOOK OF US"));
-        System.out.println("==========================================");
+        System.out.println("Menampilkan pricelist album Day6");
     }
 
-    @Override
     public void beliAlbum1(Scanner input) {
-        input.nextLine();
-        System.out.println("=================================================");
-        System.out.println("==========  PEMBELIAN ALBUM NCT DREAM  =========");
-        System.out.println("=================================================");
-        System.out.println("Idol    : " + nama);
-        System.out.print("Nama Album    : ");
-        this.album = input.nextLine();
-        this.harga = tentukanHarga(this.album);
-        System.out.println("Harga     : Rp " + this.harga);
-        System.out.print("Masukkan jumlah Album : ");
-        qty = input.nextInt();
-        total = qty * harga;
-        System.out.println("=================================================");
-        System.out.println();
-        System.out.println("Anda telat membeli album ini, Terimakasih <3");
-        System.out.println();
+        System.out.println("Input pembelian album NCT DREAM");
     }
 
-    @Override
     public void beliAlbum2(Scanner input) {
-        input.nextLine();
-        System.out.println("=================================================");
-        System.out.println("==============  PEMBELIAN ALBUM DAY6  ===========");
-        System.out.println("=================================================");
-        System.out.println("Idol    : " + nama);
-        System.out.print("Nama Album    : ");
-        this.album = input.nextLine();
-        this.harga = tentukanHarga(this.album);
-        System.out.println("Harga     : Rp " + this.harga);
-        System.out.print("Masukkan jumlah Album : ");
-        qty = input.nextInt();
-        total = qty * harga;
-        System.out.println("=================================================");
-        System.out.println();
-        System.out.println("Anda telat membeli album ini, Terimakasih <3");
-        System.out.println();
+        System.out.println("Input pembelian album Day6");
     }
 
-    @Override
     public void cetakStruk() {
-        System.out.println("=================================================");
-        System.out.println("===============  STRUK PEMBELIAN  ===============");
-        System.out.println("=================================================");        
-        System.out.println("Idol         : " + this.nama);
-        System.out.println("Album        : " + this.album);
-        System.out.println("Harga Album  : Rp " + this.harga);
-        System.out.println("Jumlah       : " + this.qty);
-        System.out.println("Total Harga  : Rp " + this.total);
-        System.out.println("================================================");
-        System.out.println();
-    }
-
-    private double tentukanHarga(String album) {
-        switch (album.toUpperCase()) {
-            case "GLITCH MODE": return 300000;
-            case "HOT SAUCE": return 250000;
-            case "FOUREVER": return 280000;
-            case "THE BOOK OF US": return 320000;
-            default:
-                System.out.println("Album tidak terdaftar, harga = 0");
-                return 0;
-        }
+        System.out.println("Menampilkan struk pembelian album");
     }
 }
