2d7e771c7c0dc14e9990818ac72e1bc267e34e30
innatfya <innalutfiah@gmail.com>
2025-11-28 18:27:17 +0700

    aktor n biodata cenah

--- src/com/proyek/manusia/ChildAktor/Aktor.java
+++ src/com/proyek/manusia/ChildAktor/Aktor.java
@@ -1,5 +1,22 @@
 package com.proyek.manusia.ChildAktor;
+import java.util.Scanner;
+import com.proyek.manusia.Agensi;
 
-public class Aktor {
-    
-}
+public class Aktor extends Agensi {
+
+    public Aktor(String nama, String kewarganegaraan, String ttlLahir,
+                  String ttlDebut, int masaTrainee) {
+        super(nama, kewarganegaraan, ttlLahir, ttlDebut, masaTrainee);}
+        
+    public void tampilkanAktor() {
+        System.out.println("Menampilkan biodata aktor");}
+    public void infoEvent() {
+        System.out.println("Menampilkan info event");}
+    public void cetakTiket() {
+        System.out.println("Menampilkan tiket");}
+    public void inputTiket(Scanner input) {
+        System.out.println("Input tiketing");}
+    public void Wuseok() {
+        System.out.println("Mengeluarkan teks selamat membeli tiket Byeon Wu Seok");}
+    public void GongYoo() {
+        System.out.println("Mengeluarkan teks selamat membeli tiket Gong Yoo");}}
--- src/com/proyek/manusia/ChildAktor/BiodataAktor.java
+++ src/com/proyek/manusia/ChildAktor/BiodataAktor.java
@@ -1,8 +1,28 @@
 package com.proyek.manusia.ChildAktor;
 
-import com.proyek.manusia.Agensi;
+public class BiodataAktor extends Aktor {
+    private String listDrama;
+    private String listMovie;
+    private String penghargaan;
+    public BiodataAktor(String nama, String kewarganegaraan, String ttlLahir,
+                         String ttlDebut, int masaTrainee, String posisi,
+                         String drama, String movie, String album, String penghargaan) {
+        super(nama, kewarganegaraan, ttlLahir, ttlDebut, masaTrainee);
+        this.listDrama = drama;
+        this.listMovie = movie;
+        this.penghargaan = penghargaan;}
 
-public class BiodataAktor extends Agensi {
-
-
-}
+    @Override
+    public void tampilkanAktor() {
+        System.out.println("=================================================");
+        System.out.println("==============   BIODATA AKTOR   ===============");
+        System.out.println("=================================================");
+        System.out.println("Nama             : " + nama);
+        System.out.println("Kewarganegaraan  : " + kewarganegaraan);
+        System.out.println("Tanggal Lahir    : " + ttlLahir);
+        System.out.println("Debut            : " + ttlDebut);
+        System.out.println("Drama            : " + listDrama);
+        System.out.println("Movie            : " + listMovie);
+        System.out.println("Penghargaan      : " + penghargaan);
+        System.out.println("=================================================\n");
+        System.out.println();}}
